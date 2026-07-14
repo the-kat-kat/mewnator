c479757a7fd40b0701c41bda548ff941ddfaee13
katiikati <166103545+the-kat-kat@users.noreply.github.com>
2026-07-14 15:01:36 -0700

    const -> let gahhh

--- src/orphs.js
+++ src/orphs.js
@@ -32,8 +32,7 @@ function isColliding(boxA, boxB) {
 export function updateOrphs(deltaTime, canvasWidth, onChomp) {
   if (!imageLoaded) return;
 
-
-  const speed = 0.3; // pixels per ms
+  let speed = 0.3; // pixels per ms
 
   timeSinceLastSpawn += deltaTime;
   if (timeSinceLastSpawn > spawnInterval) {
